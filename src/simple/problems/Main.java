d35a46dd5bcd848c593f6c5a6cf2ae7efacf7cb0
arvipDev <arvind.purushotham@gmail.com>
2020-06-12 22:30:04 +0530

    Simple problem set. Question 2 - convert roman numbers to numbers

--- src/simple/problems/Main.java
+++ src/simple/problems/Main.java
@@ -2,7 +2,14 @@ package simple.problems;
 
 public class Main {
     public static void main(String[] args) {
-        Question1 q1 = new Question1();
-        System.out.println(q1.isPalindrome(121));
+        //Question1 q1 = new Question1();
+        //System.out.println(q1.isPalindrome(121));
+
+        Question2 q2 = new Question2();
+        System.out.println(q2.romanToInt("XXXIV"));
+        System.out.println(q2.romanToInt("LVIII"));
+        System.out.println(q2.romanToInt("IX"));
+        System.out.println(q2.romanToInt("MCMXCIV"));
+        System.out.println(q2.romanToInt("DCXXI"));
     }
 }
