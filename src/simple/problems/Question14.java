b47c3d9d86f8146566c5d760048f8a1c06759c4e
arvipDev <arvind.purushotham@gmail.com>
2020-10-14 23:12:07 +0530

    Added Question 14

---
+++ src/simple/problems/Question14.java
@@ -0,0 +1,17 @@
+package simple.problems;
+
+//  Given an array of numbers arr. A sequence of numbers is called an arithmetic progression if the difference
+//  between any two consecutive elements is the same.
+//Return true if the array can be rearranged to form an arithmetic progression, otherwise, return false.
+
+//      Constraints:
+//2 <= arr.length <= 1000
+//-10^6 <= arr[i] <= 10^6
+
+public class Question14 {
+    public static void main(String[] args) {
+
+    }
+
+
+}
