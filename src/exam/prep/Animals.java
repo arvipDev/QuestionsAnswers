ea1da8b1417ef77cc818f91cf666feb4620418e0
arvipDev <arvind.purushotham@gmail.com>
2020-11-25 19:26:29 +0530

    exam prep

--- src/exam/prep/Animals.java
+++ src/exam/prep/Animals.java
@@ -1,4 +1,9 @@
 package exam.prep;
 
 public class Animals {
+    public int number;
+    String name;
+    public static void doSomething() {
+        System.out.println("doing something");
+    }
 }
--- src/exam/prep/App.java
+++ src/exam/prep/App.java
@@ -1,4 +1,11 @@
 package exam.prep;
 
 public class App {
+    public static void main(String[] args) {
+        Animals dog = new Animals();
+        dog.number = 10;
+        dog.name = "Dolly";
+        Animals.doSomething();
+        //dog.doSomething();
+    }
 }
