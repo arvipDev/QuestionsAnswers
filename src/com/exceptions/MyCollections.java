d8ed8395afa6aa59f243dfb5be1fe54a4eeafc5d
arvipDev <arvind.purushotham@gmail.com>
2020-12-03 13:03:41 +0530

    ArrayList and LinkedList

---
+++ src/com/exceptions/MyCollections.java
@@ -0,0 +1,47 @@
+package com.exceptions;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class MyCollections {
+    //Arrays, ArrayList, LinkedList
+    public static void main(String[] args) {
+        //testOne();
+        testTwo();
+    }
+
+    private static void testOne () {
+        ArrayList<String> alist = new ArrayList<>();
+        alist.add("I");
+        alist.add("am");
+        alist.add("Arvind");
+        alist.add("Purushotham");
+        alist.add("Age");
+        alist.add("29");
+        System.out.println(alist.indexOf("Arvind"));
+        //alist.remove(2);
+        alist.iterator().forEachRemaining(s -> System.out.println(s));
+        System.out.println(alist.iterator().hasNext());
+        System.out.println(alist.iterator().next());
+        //System.out.println(alist.toString());
+    }
+
+    private static void testTwo () {
+        LinkedList<String> alist = new LinkedList<>();
+        alist.add("I");
+        alist.add("am");
+        alist.add("Arvind");
+        alist.add("Purushotham");
+        alist.add("Age");
+        alist.add("29");
+        System.out.println(alist.indexOf("Arvind"));
+        System.out.println(alist.element());
+        System.out.println(alist.offer("years"));
+        alist.iterator().forEachRemaining(s -> System.out.println(s));
+        alist.remove();
+        alist.iterator().forEachRemaining(s -> System.out.println(s));
+        alist.addAll(6, alist);
+        System.out.println("*****************");
+        alist.iterator().forEachRemaining(s -> System.out.println(s));
+    }
+}
