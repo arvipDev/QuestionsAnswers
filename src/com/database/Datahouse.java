1df6f2ab4486898da2f4a58d13ef3669bf2de5a2
arvipDev <arvind.purushotham@gmail.com>
2020-12-17 10:35:15 +0530

    CRUD to MySQL DB table

--- src/com/database/Datahouse.java
+++ src/com/database/Datahouse.java
@@ -5,7 +5,12 @@ import java.sql.*;
 public class Datahouse {
 
     public static void main(String[] args) {
+        testFour();
+    }
 
+    private static void testOne () {
+
+        // retrieve data from MySQL database, table employees_tbl
         String url = "jdbc:mysql://127.0.0.1:3306/employee_database";
         try {
             Connection conn = DriverManager.getConnection(url, "root", "");
@@ -21,4 +26,58 @@ public class Datahouse {
             e.printStackTrace();
         }
     }
+
+    private static void testTwo () {
+
+        //Inserting a record(row) into table.
+        String url = "jdbc:mysql://127.0.0.1:3306/employee_database";
+        int value;
+
+        try {
+            Connection conn = DriverManager.getConnection(url, "root", "");
+            Statement statement = conn.createStatement();
+            value = statement.executeUpdate("insert into employees_tbl (id, name, dept, salary) " +
+                    "values(1245, 'Arvind', 'Science', 25000)");
+            if (value == 1)
+                System.out.println("Successfully inserted a record into the table");
+        } catch (SQLException e) {
+            System.out.println("Error trying to insert a record into the table");
+            e.printStackTrace();
+        }
+    }
+
+    private static void testThree () {
+
+        //Removing a record(row) from table.
+        String url = "jdbc:mysql://127.0.0.1:3306/employee_database";
+        int value;
+
+        try {
+            Connection conn = DriverManager.getConnection(url, "root", "");
+            Statement statement = conn.createStatement();
+            value = statement.executeUpdate("delete from employees_tbl where name='alex'");
+            System.out.println("removing row " + value);
+        } catch (SQLException e) {
+            System.out.println("Error trying to insert a record into the table");
+            e.printStackTrace();
+        }
+    }
+
+    private static void testFour () {
+
+        //Removing a (attribute)column from table.
+        String url = "jdbc:mysql://127.0.0.1:3306/employee_database";
+        int value;
+
+        try {
+            Connection conn = DriverManager.getConnection(url, "root", "");
+            Statement statement = conn.createStatement();
+            value = statement.executeUpdate("alter table employees_tbl drop column name");
+            System.out.println("removing column " + value);
+        } catch (SQLException e) {
+            System.out.println("Error trying to insert a record into the table");
+            e.printStackTrace();
+        }
+    }
+
 }
