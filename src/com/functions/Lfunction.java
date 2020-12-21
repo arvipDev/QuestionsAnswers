d6ca21fc1bfe66a1d98055482b1561f2f56ffa61
arvipDev <arvind.purushotham@gmail.com>
2020-12-21 12:36:03 +0530

    Passign arguments to Lambda functions

--- src/com/functions/Lfunction.java
+++ src/com/functions/Lfunction.java
@@ -2,7 +2,14 @@ package com.functions;
 
 public class Lfunction {
     public static void main(String[] args) {
+        //testOne();
+        testThree();
+    }
+    private static void runner (Runnable runner) {
+        runner.run();
+    }
 
+    private static void testOne () {
         // Passing an implementation of the interface as an argument to a method whose parameter is of type interface.
         runner(new Human());
 
@@ -25,8 +32,22 @@ public class Lfunction {
             System.out.println(" - Second line");
         });
     }
-    private static void runner (Runnable runner) {
+
+    private static void testTwo () {
+        // Creating an object of the functional interface and assigning a lambda function to it.
+        Runnable runner = () -> System.out.println("running...");
         runner.run();
     }
 
+    private static void testThree () {
+        // Creating an object of functional interface and assigning a lambda function that takes in an argument of type string.
+        Livable live = (s) -> System.out.println(s);
+        live.live("I am a survivor..");
+    }
+
 }
+
+interface Livable {
+    public void live(String string);
+}
+
