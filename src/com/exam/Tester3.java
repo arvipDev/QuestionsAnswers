bc664fd9904f30a29dc4ff05b3b93ebac6853cc5
arvipDev <arvind.purushotham@gmail.com>
2021-01-06 19:33:30 +0530

    Test 3 review complete

--- src/com/exam/Tester3.java
+++ src/com/exam/Tester3.java
@@ -1,14 +1,30 @@
 package com.exam;
 
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Tester3 {
 
     public static void main(String[] args) {
-        q2();
+        q3();
+
+    }
+
+    private static void q3() {
+        final int i1 = 1;
+        final Integer i2 = 1;
+        final String s = ":ONE";
+        //final String s = new String(":ONE"); //will return false for both.
+
+        String s1 = i1 + s;
+        String s2 = i2 + s;
+
+        System.out.println(s1 == "1:ONE");
+        System.out.println(s2 == "1:ONE");
+        //System.out.println(s2.equals("1:ONE")); // will return true
     }
 
     private static void q2(){
