cb66f00c93523a14c36d73e7ae84420227119d6b
arvipDev <arvind.purushotham@gmail.com>
2021-06-17 12:03:42 +0530

    regex

---
+++ src/com/regex/Main.java
@@ -0,0 +1,12 @@
+package com.regex;
+
+public class Main {
+
+    public static void main(String[] args) {
+        test1();
+    }
+
+    public static void test1(){
+        System.out.println("Regex tests");
+    }
+}
